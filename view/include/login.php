4fe800acb0cdf1ba28e4e2e2a9c01734ac90fd35
WalsonK <kevin.walz@imie-paris.fr>
2020-09-17 20:24:26 +0200

    Organisation projet

---
+++ view/include/login.php
@@ -0,0 +1,36 @@
+<main>
+    <div class="container">
+        <div class="row">
+            <!-- Insert logo here -->
+        </div>
+
+        <div class="row mt-5">
+            <!-- Login Card -->
+            <div class="card col-8 mx-auto" id="loginCard">
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="emailInput">Adresse e-mail Professionnel</label>
+                            <input type="email" class="form-control" id="EmailInput">
+                        </div>
+                        <div class="form-group">
+                            <label for="passwordInput">Mot de passe</label>
+                            <input type="password" class="form-control" id="passwordInput">
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="saveCheck">
+                            <label class="form-check-label" for="saveCheck">Sauvegarder</label>
+                        </div>
+                        <div class="row">
+                            <div class="col-auto mx-auto">
+                                <button type="submit" class="btn btn-primary">
+                                    Se connecter
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
